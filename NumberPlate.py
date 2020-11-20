ab1912c3c9030d49b7fc20a5e8eb24a8c14948e7
namangeda <namang135@gmail.com>
2020-11-20 11:33:36 +0530

    No. plate
    NO.plate detection using OpenCv

---
+++ NumberPlate.py
@@ -0,0 +1,26 @@
+from cv2 import cv2
+
+platecascade = cv2.CascadeClassifier(r"C:\python\pythonProject\opencv\data\haarcascade_russian_plate_number.xml")
+frameWidth  = 640
+frameHeight = 400
+livecap = cv2.VideoCapture(0)
+livecap.set(3, frameWidth)      
+livecap.set(4, frameHeight)
+livecap.set(10, 150)
+count = 0
+while True:
+    success, img = livecap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    plates = platecascade.detectMultiScale(imgGray,1.1,4)
+    for (x, y, w, h)  in plates:
+        area = w*h
+        if area > 500:
+            cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.putText(img, "number_plate", (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0,255,0), 1)
+            plateArea = img[y:y+h,x:x+w]
+            cv2.imshow("live cam", plateArea)
+    cv2.imshow("live cam", img)        
+    if cv2.waitKey(1) & 0xFF ==ord('s'):     # once it detected numeber plate press s to save the plate imge in directory as written in imwrite
+        cv2.imwrite(r"C:\python\pythonProject\opencv\data\scannedPlates\plate_"+str(count)+".jpg",plateArea)
+        count+= 1
+        
---
+++ README.txt
@@ -0,0 +1,6 @@
+its a very simple python (openCV) program
+to detect no. plates from cars through live cam
+
+
+go check it out
+simply run NumberPlate.py
